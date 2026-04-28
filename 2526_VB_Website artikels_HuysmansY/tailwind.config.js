1c1d7bfd91884225a69da5e8456491c7983f5d14
yindrah <s169019@ap.be>
2026-04-29 01:55:45 +0200

    Delete 2526_VB_Website artikels_HuysmansY directory

--- 2526_VB_Website artikels_HuysmansY/tailwind.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './data/**/*.{js,jsx}'], theme: { extend: { fontFamily: { serif: ['Georgia','serif'], sans: ['Inter','Arial','sans-serif'] } } }, plugins: [] }
